8a086b270ac8872a356f0e429d9a26cde3000db0
htnha <hoang.tuan.nha@gmail.com>
2024-12-24 09:43:10 +0700

    test file

--- bot1.py
+++ bot1.py
@@ -1,12 +1,12 @@
 # Done! Congratulations on your new bot. You will find it at t.me/K64qbu_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 #
 # Use this token to access the HTTP API:
-# 8192359925:AAEeuvgX2TwnQi-eEGZg_PXKTNeIS48dgjw
+# 8192359925:AAEeuvgX2TwnQi-eEGZg
 # Keep your token secure and store it safely, it can be used by anyone to control your bot.
 #
 # For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 # Replace with your Bot Token from BotFather
-BOT_TOKEN = '8192359925:AAEeuvgX2TwnQi-eEGZg_PXKTNeIS48dgjw'
+BOT_TOKEN = '8192359925:AAEeuvgX2TwnQi-eEGZg'
 REGISTER_FILE = 'register.txt'
 
 from telegram import Update
