3f7d2c4e6807a32a561f5272b0f993b552400e3b
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-12-31 15:55:19 +0700

    student dung main

--- bt1/main.py
+++ bt1/main.py
@@ -1,7 +1,8 @@
 from student_Nha import Nha
 from student_Long import Long
+from student_Dung import Dung
 # Create a list of animals
-k64_class = [Nha(),Long(),]
+k64_class = [Nha(),Long(),Dung()]
 
 # Polymorphic behavior
 for student in k64_class:
