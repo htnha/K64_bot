01dd181b499d12daeac5bbf442a1611a98c00cdf
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-12-31 16:50:01 +0700

    dung

--- bt1/student_Dung.py
+++ bt1/student_Dung.py
@@ -1,6 +1,8 @@
 from student import Student
 
 class Dung(Student):
+    def name(self):
+        return "Dung"
     def speak(self):
         return "Toi len la: Dung \nEmail: sevensoderfive@gmail.com"
     def address(self):
