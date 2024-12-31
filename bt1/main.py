06b426df14b060c40890f69692afbcb356321601
EKALAT <koongxd66@gmail.com>
2024-12-31 15:54:08 +0700

    update

--- bt1/main.py
+++ bt1/main.py
@@ -1,7 +1,9 @@
 from student_Nha import Nha
 from student_Long import Long
+from student_Ekalat import Ekalat
 # Create a list of animals
 k64_class = [Nha(),Long(),]
+k64_class = [Nha(),Ekalat(),]
 
 # Polymorphic behavior
 for student in k64_class:
