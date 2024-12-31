ebef5044a0f356fb086cbbb3f658d7b4445516a1
nguyenphong <152574718+Socsaohoa12@users.noreply.github.com>
2024-12-31 16:11:38 +0700

    Resolved merge conflicts

--- bt1/main.py
+++ bt1/main.py
@@ -3,7 +3,8 @@ from student_Long import Long
 from student_Ekalat import Ekalat
 from student_Dung import Dung
 from student_Tham import Tham
+from student_Phong import Phong
 # Create a list of animals
-k64_class = [Nha(),Long(),Dung(), Ekalat(), Tham()]
+k64_class = [Nha(),Long(),Dung(), Ekalat(), Tham(), Phong()]
 for student in k64_class:
     print(student.speak())
---
+++ bt1/student_Phong.py
@@ -0,0 +1,5 @@
+from student import Student
+
+class Phong(Student):
+    def speak(self):
+        return "Toi len la: Phong"
