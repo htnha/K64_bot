4df63cfeced4b901aa373e12eaa42626e4370ce1
EKALAT <koongxd66@gmail.com>
2024-12-31 17:00:58 +0700

    update

--- bt1/main.py
+++ bt1/main.py
@@ -5,12 +5,14 @@ from student_Dung import Dung
 from student_Tham import Tham
 from student_Linh import Linh
 from student_Phong import Phong
-# Create a list of animals
-k64_class = [Nha(),Long(),Dung(), Ekalat(), Tham(), Linh(),Phong()]
+
+# Create a list of students
+k64_class = [Nha(), Long(), Dung(), Ekalat(), Tham(), Linh(), Phong()]
+
 for student in k64_class:
     try:
         print(student.name())
         print(student.speak())
         print(student.address())
-    except:
-        print("Co loi cho sinh vien:" + student.name())
+    except NotImplementedError as e:
+        print(f"Co loi cho sinh vien: {type(student).__name__} - {e}")
