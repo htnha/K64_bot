d76a8f0e2deb001ace8b9559fdc538a5f279e030
nguyenvantham12345 <160579622+nguyenvantham12345@users.noreply.github.com>
2025-01-06 17:08:12 +0700

    Update student_Tham.py

--- bt1/student_Tham.py
+++ bt1/student_Tham.py
@@ -1,5 +1,17 @@
 from student import Student
 
 class Tham(Student):
+    def name(self):
+        return "Tham"
     def speak(self):
-        return "Toi len la: Tham"
+        return "Toi len la: Tham \nEmail: nguyenvantham.vtabd@gmail.com"
+    def address(self):
+        return "Quang Trachnt import Student
+
+class Tham(Student):
+    def name(self):
+        return "Tham"
+    def speak(self):
+        return "Toi len la: Tham \nEmail: nguyenvantham.vtabd@gmail.com"
+    def address(self):
+        return "Quang Trach country"
