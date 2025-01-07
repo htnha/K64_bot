806b0f1a440e47dfe574667facc3cdfe7311dbba
dinhcaolinh2004 <dinhcaolinh2004@gmail.com>
2025-01-07 11:10:12 +0700

    student_linh.py

--- bt1/student_Linh.py
+++ bt1/student_Linh.py
@@ -1,5 +1,9 @@
 from student import Student
 
 class Linh(Student):
+    def name(self):
+        return "Linh"
     def speak(self):
-        return "Toi len la: Linh"
+        return "Toi len la: Linh \nEmail: dinhcaolinh73qb@gmail.com"
+    def address(self):
+        return "DH city"
