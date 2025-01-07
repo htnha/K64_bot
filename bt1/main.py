cf6ea32dabd95304c46ef638e2882b64c505ecf3
ten cua ban <149994716+baodat946@users.noreply.github.com>
2025-01-07 23:53:13 +0700

    Đạt

--- bt1/main.py
+++ bt1/main.py
@@ -5,9 +5,10 @@ from student_Ekalat import Ekalat
 from student_Tham import Tham
 from student_Linh import Linh
 from student_Phong import Phong
+from student_Dat import Dat
 
 # Create a list of students
-k64_class = [Nha(), Long(), Dung(), Ekalat(), Tham(), Linh(), Phong()]
+k64_class = [Nha(), Long(), Dung(), Ekalat(), Tham(), Linh(), Phong(), Dat()]
 
 for student in k64_class:
     try:
---
+++ bt1/student_Dat.py
@@ -0,0 +1,9 @@
+from student import Student
+
+class Dat(Student):
+    def name(self):
+        return "Pham Nguyen Bao Dat"
+    def speak(self):
+        return "Toi len la: Đạt.\nGmail: phamnguyenbaodat946@gmail.com"
+    def address(self):
+        return "Dong Hoi-Quang Binh"
